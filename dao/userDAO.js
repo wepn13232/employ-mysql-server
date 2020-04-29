f1f304f50d982aafaada462ff5fb1a94f4a7b458
howe94 <996341242@qq.com>
2020-04-30 00:08:12 +0800

    修改页码

--- dao/userDAO.js
+++ dao/userDAO.js
@@ -3,54 +3,55 @@ var mysqlConf = require('../conf/mysqlConf');
 var userSqlMap = require('./userSqlMap');
 var pool = mysql.createPool(mysqlConf.mysql);
 module.exports = {
-    add: function (user, callback) {
-        pool.query(userSqlMap.add, [user.userId, user.userName, user.userType, user.password,user.session,'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1811580024,2009798947&fm=26&gp=0.jpg'], function (error, result) {
-            if (error) throw error;
-            callback(result.affectedRows > 0);
-        });
-    },
-    list: function (pageObj,callback) {
-        console.log((pageObj.currPage-1)*pageObj.pageRowNum,pageObj.pageRowNum)
-        pool.query(userSqlMap.list,[(pageObj.currPage-1)*pageObj.pageRowNum,pageObj.pageRowNum*1], function (error, result) {
-            if (error) throw error;
-            callback(result);
-        });
-    },
-    getByUserId: function (userId, callback) {
-        pool.query(userSqlMap.getByUserId, userId, function (error, result) {
-            if (error) throw error;
-            console.log(result)
-            callback(result);
-        });
-    },
-    deleteById: function (id, callback) {
-        pool.query(userSqlMap.deleteById, id, function (error, result) {
-            if (error) throw error;
-            callback(result.affectedRows > 0);
-        });
-    },
-    update: function (user, callback) {
-        pool.query(userSqlMap.update, [user.userName, user.password, user.id], function (error, result) {
-            if (error) throw error;
-            callback(result.affectedRows > 0);
-        });
-    },
-    updateImage: function (user, callback) {
-        pool.query(userSqlMap.updateImage, [user.filePath, user.userId], function (error, result) {
-            if (error) throw error;
-            callback(result.affectedRows > 0);
-        });
-    },
-    login: function (userId, callback) {
-        pool.query(userSqlMap.login, userId, function (error, result) {
-            if (error) throw error;
-            callback(result)
-            // if (result.length === 0) {
-            //     callback(result)
-            // } else {
-            //     callback(result[0]);
-            // }
+  add: function (user, callback) {
+    pool.query(userSqlMap.add, [user.userId, user.userName, user.userType, user.password, user.session, 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1811580024,2009798947&fm=26&gp=0.jpg'], function (error, result) {
+      if (error) throw error;
+      callback(result.affectedRows > 0);
+    });
+  },
+  list: function (pageObj, callback) {
+    console.log((pageObj.currPage - 1) * pageObj.pageRowNum, pageObj.pageRowNum)
+    pool.query(userSqlMap.list, [(pageObj.currPage - 1) * pageObj.pageRowNum, pageObj.pageRowNum * 1], function (error, result) {
+      if (error) throw error;
+      console.log(result)
+      callback(result);
+    });
+  },
+  getByUserId: function (userId, callback) {
+    pool.query(userSqlMap.getByUserId, userId, function (error, result) {
+      if (error) throw error;
+      console.log(result)
+      callback(result);
+    });
+  },
+  deleteById: function (id, callback) {
+    pool.query(userSqlMap.deleteById, id, function (error, result) {
+      if (error) throw error;
+      callback(result.affectedRows > 0);
+    });
+  },
+  update: function (user, callback) {
+    pool.query(userSqlMap.update, [user.userId, user.userName, user.userType, user.password, user.filePath, user.id], function (error, result) {
+      if (error) throw error;
+      callback(result.affectedRows > 0);
+    });
+  },
+  updateImage: function (user, callback) {
+    pool.query(userSqlMap.updateImage, [user.filePath, user.userId], function (error, result) {
+      if (error) throw error;
+      callback(result.affectedRows > 0);
+    });
+  },
+  login: function (userId, callback) {
+    pool.query(userSqlMap.login, userId, function (error, result) {
+      if (error) throw error;
+      callback(result)
+      // if (result.length === 0) {
+      //     callback(result)
+      // } else {
+      //     callback(result[0]);
+      // }
 
-        })
-    }
+    })
+  }
 };
--- routes/users.js
+++ routes/users.js
@@ -10,171 +10,207 @@ var fs = require('fs');
 
 /* list users */
 router.get('/', function (req, res) {
-    var pageObj = req.query;
-    console.log('list users called',pageObj);
-    userDao.list(pageObj,function (users) {
-        pageObj.totalRow = users[1].totalRow
-        dataList = {
-            userList:users[0],
-            pageObj
-        }
-        res.json(result.createResult('get', true, dataList));
-    });
+  var pageObj = req.query;
+  console.log('list users called', pageObj);
+  userDao.list(pageObj, function (users) {
+    pageObj.totalRow = users[1][0].totalRow;
+    pageObj.currPage = pageObj.currPage * 1;
+    pageObj.pageRowNum = pageObj.pageRowNum * 1;
+    dataList = {
+      userList: users[0],
+      pageObj
+    }
+    res.json(result.createResult('get', true, dataList));
+  });
 });
 
 /* get user */
 router.get('/:userId', function (req, res) {
-    var userId = req.params.userId;
-    console.log('get user called, userId: ' + userId);
-    userDao.getByUserId(userId, function (user) {
-        // console.log("===============")
-        // console.log(user[0])
-        delete user[0].password //删除密码
-        res.json(result.createResult('get', true, user[0]));
-    });
+  var userId = req.params.userId;
+  console.log('get user called, userId: ' + userId);
+  userDao.getByUserId(userId, function (user) {
+    // console.log("===============")
+    // console.log(user[0])
+    delete user[0].password //删除密码
+    res.json(result.createResult('get', true, user[0]));
+  });
 });
 
 
 /* delete user */
 router.delete('/:id', function (req, res) {
-    var id = req.params.id;
-    console.log('delete user called, id=' + id);
-    userDao.deleteById(id, function (success) {
-        res.json(result.createResult('delete', success, null));
-    });
+  var id = req.params.id;
+  console.log('delete user called, id=' + id);
+  userDao.deleteById(id, function (success) {
+    res.json(result.createResult('delete', success, null));
+  });
 });
 
 /* add users */
 router.post('/', function (req, res) {
-    console.log('post users called');
-    var user = req.body;
-    userDao.getByUserId(user.userId, function (userList) {
-        if (userList.length == 0) {
-            userDao.add(user, function (success) {
-                // manageBaseDao.addStuInfo(user)
-                var r = result.createResult('post', success, null);
-                res.json(r);
-            });
-        } else {
-            res.json({
-                status: '401',
-                message: "用户数据已存在",
-                data: []
-            });
-        }
-    })
+  console.log('post users called');
+  var user = req.body;
+  userDao.getByUserId(user.userId, function (userList) {
+    if (userList.length == 0) {
+      userDao.add(user, function (success) {
+        // manageBaseDao.addStuInfo(user)
+        var r = result.createResult('post', success, null);
+        res.json(r);
+      });
+    } else {
+      res.json({
+        status: '401',
+        message: "用户数据已存在",
+        data: []
+      });
+    }
+  })
 
 });
 /* post user by userId and password*/
 router.post('/login', function (req, res) {
-    var userId = req.body.userId;
-    var password = req.body.password;
-    console.log('post user called, userId: ' + userId);
-    userDao.login(userId, function (user) {
-        if (user.length == 0) {
-            res.json({
-                status: '401',
-                message: '账户或者密码有误!',
-                data: []
-            })
-        } else {
-            var key = "ILOVEKUANGMINYI";
-            var save_password = aesDecrypt(user[0].password, key);
-            if (save_password == aesDecrypt(password, key)) {
-                delete user[0].password //删除密码
-                res.json({
-                    status: '200',
-                    message: '登陆成功!',
-                    data: user[0]
-                });
+  var userId = req.body.userId;
+  var password = req.body.password;
+  console.log('post user called, userId: ' + userId);
+  userDao.login(userId, function (user) {
+    if (user.length == 0) {
+      res.json({
+        status: '401',
+        message: '账户或者密码有误!',
+        data: []
+      })
+    } else {
+      var key = "ILOVEKUANGMINYI";
+      var save_password = aesDecrypt(user[0].password, key);
+      if (save_password == aesDecrypt(password, key)) {
+        delete user[0].password //删除密码
+        res.json({
+          status: '200',
+          message: '登陆成功!',
+          data: user[0]
+        });
 
-            }
-        }
-    });
+      }
+    }
+  });
 });
 
 /* update users */
 router.put('/:id', function (req, res) {
-    console.log('update users called');
-    var user = req.body;
-    // user = req.params;
-    console.log(user);
-    userDao.update(user, function (success) {
-        var r = result.createResult('put', success, null);
-        res.json(r);
+  console.log('update users called');
+  var uploadDir = './public/employSystem/img/';
+  var viewDir = './img/'
+  var form = new formidable.IncomingForm();
+  //文件的编码格式
+  form.encoding = 'utf-8';
+  //文件的上传路径
+  form.uploadDir = uploadDir;
+  //文件的后缀名
+  form.extensions = true;
+  //文件的大小限制
+  form.maxFieldsSize = 2 * 1024 * 1024;
+  form.parse(req, function (err, fields, files) {
+    //fields上传的string类型的信息
+    //files为上传的文件
+    var userParams = fields;
+    var avatar = files.avatar;
+    var oldpath = path.normalize(avatar.path); //返回正确格式的路径
+
+    var newfilename = userParams.userId + avatar.name;
+    var newpath = uploadDir + newfilename;
+
+    //写入数据库的信息
+    var userInfo = {
+      userParams,
+      filePath: viewDir + newfilename
+    }
+    //将老的图片路径改为新的图片路径
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) {
+        console.error("改名失败" + err);
+      } else {
+        // userInfo.filePath = newpath;
+        userDao.update(userInfo, function (success) {
+          var r = result.createResult('put', success, null);
+          res.json(r);
+        });
+      }
     });
+  })
+
+
 });
 
 /* patch users */
 router.patch('/:id', function (req, res) {
-    console.log('patch users called');
-    userDao.getById(req.params.id, function (user) {
-        var userName = req.body.userName;
-        if (userName) {
-            user.userName = userName;
-        }
-        var password = req.body.password;
-        if (password) {
-            user.password = password;
-        }
-        console.log(user);
-        userDao.update(user, function (success) {
-            var r = result.createResult('patch', success, null);
-            res.json(r);
-        });
+  console.log('patch users called');
+  userDao.getById(req.params.id, function (user) {
+    var userName = req.body.userName;
+    if (userName) {
+      user.userName = userName;
+    }
+    var password = req.body.password;
+    if (password) {
+      user.password = password;
+    }
+    console.log(user);
+    userDao.update(user, function (success) {
+      var r = result.createResult('patch', success, null);
+      res.json(r);
     });
+  });
 });
 
 //上传个人头像图片
 router.post('/updateUserImage', function (req, res) {
-    var uploadDir = './public/employSystem/img/';
-    var viewDir = './img/'
-    var form = new formidable.IncomingForm();
-    //文件的编码格式
-    form.encoding = 'utf-8';
-    //文件的上传路径
-    form.uploadDir = uploadDir;
-    //文件的后缀名
-    form.extensions = true;
-    //文件的大小限制
-    form.maxFieldsSize = 2 * 1024 * 1024;
-    form.parse(req, function (err, fields, files) {
-        //fields上传的string类型的信息
-        //files为上传的文件
-        var userId = fields.userId;
-        var avatar = files.avatar;
-        var oldpath = path.normalize(avatar.path); //返回正确格式的路径
+  var uploadDir = './public/employSystem/img/';
+  var viewDir = './img/'
+  var form = new formidable.IncomingForm();
+  //文件的编码格式
+  form.encoding = 'utf-8';
+  //文件的上传路径
+  form.uploadDir = uploadDir;
+  //文件的后缀名
+  form.extensions = true;
+  //文件的大小限制
+  form.maxFieldsSize = 2 * 1024 * 1024;
+  form.parse(req, function (err, fields, files) {
+    //fields上传的string类型的信息
+    //files为上传的文件
+    var userId = fields.userId;
+    var avatar = files.avatar;
+    var oldpath = path.normalize(avatar.path); //返回正确格式的路径
 
-        var newfilename = userId + avatar.name;
-        var newpath = uploadDir + newfilename;
+    var newfilename = userId + avatar.name;
+    var newpath = uploadDir + newfilename;
 
-        //写入数据库的信息
-        var userInfo = {
-            userId: userId,
-            filePath: viewDir + newfilename
-        }
-        //将老的图片路径改为新的图片路径
-        fs.rename(oldpath, newpath, function (err) {
-            if (err) {
-                console.error("改名失败" + err);
-            } else {
-                // userInfo.filePath = newpath;
-                userDao.updateImage(userInfo, function (success) {
-                    var r = result.createResult('put', success, null);
-                    res.json(r);
-                });
-            }
+    //写入数据库的信息
+    var userInfo = {
+      userId: userId,
+      filePath: viewDir + newfilename
+    }
+    //将老的图片路径改为新的图片路径
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) {
+        console.error("改名失败" + err);
+      } else {
+        // userInfo.filePath = newpath;
+        userDao.updateImage(userInfo, function (success) {
+          var r = result.createResult('put', success, null);
+          res.json(r);
         });
-    })
+      }
+    });
+  })
 
 });
 
 
 module.exports = router;
 
-function aesDecrypt(encrypted, key) {
-    const decipher = crypto.createDecipher('aes192', key);
-    var decrypted = decipher.update(encrypted, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
+function aesDecrypt (encrypted, key) {
+  const decipher = crypto.createDecipher('aes192', key);
+  var decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
 }
